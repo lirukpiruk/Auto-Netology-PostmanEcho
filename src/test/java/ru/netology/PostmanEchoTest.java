b5baa60fd3c8e045114bf226156739adb4268f7c
Lira Bulatova <bulatova-lira@mail.ru>
2022-08-21 23:51:26 +0300

    fail test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -20,6 +20,6 @@ class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello"));
+                .body("data", equalTo("ello"));
     }
 }
